e1f679e683c8c175976a8b6c12b989274418c55e
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-07-16 14:44:13 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class a {
+  
+         public static void main(String[] args){
+   
+    Scanner sc=new Scanner(System.in);
+    char b=sc.next().charAt(0);
+   if( b=='a'||b=='e'|| b=='i'||b=='0'||b=='u')
+  
+        
+    {
+     System.out.println("vowel");
+}
+       else {
+       System.out.println("consonant");
+    }
+    }
+}
+
+    
+
