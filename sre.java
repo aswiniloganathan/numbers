a03172f55f939f06f804392b8983234e32e4a10e
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-07-16 12:32:30 +0530

    Add files via upload

---
+++ sre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class sre {
+  
+    public static void main(String[] args)
+            
+    {
+          int a = 5;
+        if(a%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+            System.out.println("odd");
+    }
+    
+}
