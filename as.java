99e87a03d2f35f606f6c72376bb5c02dd5f0e557
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-07-16 17:14:23 +0530

    Add files via upload

---
+++ as.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class as {
+    public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      String s1;
+      String S2 = new String();
+      
+    String S1=sc.next();
+      S2=new StringBuffer(S1).reverse().toString();
+      
+     
+     
+      System.out.println("String after reverse "+S2);
+   }
+}
+    
+   
+    
+
