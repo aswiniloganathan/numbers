7e91763779534a09ad1deafabf8716635eb09c1a
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-07-16 16:55:16 +0530

    Add files via upload

---
+++ oam.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class oam {
+   
+   public static void main(String[] args){
+      String string="abcdef";
+      String reverse = new StringBuffer(string).
+      reverse().toString();
+      System.out.println("\nString before reverse:"+string);
+      System.out.println("String after reverse "+reverse);
+   }
+}
+    
+   
+
