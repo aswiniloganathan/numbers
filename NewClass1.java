9772ec0608b1b206a87f8b43ff83a4c7aa80010b
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-07-16 17:30:06 +0530

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class NewClass1 {
+    public static void main(String[] args)
+    {
+        String S1="hello";
+        System.out.println(S1);
+           System.out.println(S1);
+              System.out.println(S1);
+                 System.out.println(S1);
+                    System.out.println(S1);
+    }
+    
+}
