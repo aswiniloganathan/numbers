9d64c7d24b0cd531cbf5185b69014d55d202fdb3
aswiniloganathan <aswiniloganathan@users.noreply.github.com>
2016-08-06 18:04:25 +0530

    Add files via upload

---
+++ secnd.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Student
+ */
+public class secnd {
+      public static void main(String args[]) {
+          Scanner sc = new Scanner(System.in);
+ 
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+ 
+        String w = "";
+        String largeWord = "";
+        int i, l, lw, max = 0, c = 0;
+        char m;
+        s= s + " ";
+        l = s.length();
+        for (i = 0; i < l; i++) {
+            m = s.charAt(i);
+            if (m!= ' ') {
+                w = w+ m;
+            } else {
+                lw = w.length();
+                if (lw > max) {
+                    max = lw;
+                    largeWord = w;
+                }
+                w = "";
+            }
+        }
+        System.out.println("Largest Word: " + largeWord);    
+}
+}
+
+    
+
